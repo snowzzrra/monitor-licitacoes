8182f8f29bdca17e73f25b5dd1bebda597c26ca9
snowzzrra <guimotta7@gmail.com>
2025-08-06 14:37:45 -0300

    fix: build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,14 @@
 # Define a saída para ser detalhada e parar em caso de erro
 set -ex
 
-# 1. Instala as dependências do Python
-echo "Instalando dependências do Python..."
-python3.9 -m pip install -r requirements.txt
+# Vercel já instalou as dependências do Python a partir do requirements.txt.
+# A única tarefa deste script é instalar o Google Chrome e o Chromedriver.
 
-# 2. Instala as dependências do sistema para o Chrome
+# 1. Instala as dependências do sistema para o Chrome
 echo "Instalando dependências do sistema para o Chrome..."
 yum install -y alsa-lib atk at-spi2-atk cups-libs GConf2 gtk3 libX11-xcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libXrandr libXScrnSaver libXtst pango liberation-sans-fonts xdg-utils
 
-# 3. Baixa e instala o Google Chrome
+# 2. Baixa e instala o Google Chrome
 echo "Baixando Google Chrome..."
 wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/127.0.6533.72/linux64/chrome-linux64.zip -q -O chrome.zip
 unzip -q chrome.zip
@@ -19,12 +18,11 @@ rm chrome.zip
 mv chrome-linux64 /opt/google/chrome
 echo "Google Chrome instalado."
 
-# 4. Baixa, instala e torna executável o Chromedriver em um caminho padrão
+# 3. Baixa, instala e torna executável o Chromedriver em um caminho padrão
 echo "Baixando e instalando Chromedriver..."
 wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/127.0.6533.72/linux64/chromedriver-linux64.zip -q -O chromedriver.zip
 unzip -q chromedriver.zip
 rm chromedriver.zip
-# Move o executável para um diretório de sistema (PATH)
 mv chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
 chmod +x /usr/local/bin/chromedriver
 echo "Chromedriver instalado em /usr/local/bin/chromedriver."
