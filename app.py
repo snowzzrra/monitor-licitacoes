c3f59961dcf06bcb0fef0d89792cb1cdff3c7356
Guilherme Paim Motta <guimotta7@gmail.com>
2025-08-08 15:27:35 -0300

    branch: no-deploy

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def notificar_todos_usuarios(mensagem):
 def buscar_licitacoes_por_data(data_busca):
     # (Esta função permanece a mesma)
     options = webdriver.ChromeOptions()
-    #options.add_argument('--headless=new')
+    options.add_argument('--headless=new')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
     options.add_argument("--window-size=1920,1080")
     options.add_argument('--disable-blink-features=AutomationControlled')
